3366d1a536c19a17cedb650d8aeead184752e179
sakir tufan <sakirtufan49@gmail.com>
2021-02-13 17:01:31 +0100

    Add SemanticUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
 const App = () => {
   return (
     <div>
       <h1>GEVEZE</h1>
+      <Button primary>semantic button</Button>
     </div>
   )
 }
