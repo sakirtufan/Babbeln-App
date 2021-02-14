8f8b6b96ed792000729069e19564a4eaa3b75706
sakir tufan <sakirtufan49@gmail.com>
2021-02-14 14:14:55 +0100

    Create Redux Store

--- src/index.js
+++ src/index.js
@@ -1,25 +1,46 @@
 import React from "react";
-import 'semantic-ui-css/semantic.min.css'
-import firebase from "./firebase"
+import "semantic-ui-css/semantic.min.css";
+import store from "./store/store";
+import firebase from "./firebase";
+import { Provider } from "react-redux";
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer,
+} from "react-redux-firebase";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import Signup from "./components/auth/Signup"
-import Login from "./components/auth/Login"
+import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users",
+};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+};
 
 const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route exact path="/signup" component={Signup} />
-      <Route exact path="/login" component={Login} />
-    </Switch>
-  </Router>
+  <Switch>
+    <Route exact path="/" component={App} />
+    <Route exact path="/signup" component={Signup} />
+    <Route exact path="/login" component={Login} />
+  </Switch>
 );
 
 ReactDOM.render(
-  <Root />,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <Router>
+        <Root />
+      </Router>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
 
   document.getElementById("root")
 );
