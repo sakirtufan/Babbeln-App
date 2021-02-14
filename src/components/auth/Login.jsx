3c3bd93c1574b9201d08df1e9e456f0f7ec0314c
sakir tufan <sakirtufan49@gmail.com>
2021-02-14 12:26:22 +0100

    Add Form Validation and Error Messages

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button, Form, Grid, Segment, Message } from "semantic-ui-react";
+import { useForm } from "react-hook-form";
 import styles from "./login.module.css";
 
 const Login = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const { register, errors, handleSubmit, setValue } = useForm();
+
+  useEffect(() => {
+    register({ name: "email" }, { required: true });
+    register({ name: "password" }, { required: true, minLength: 6 });
+  }, [register]);
+
+  const onSubmit = (data, e) => {
+    console.log(data);
   };
   return (
     <Grid
@@ -18,24 +26,46 @@ const Login = () => {
           Babbeln
           <span>.io</span>
         </h1>
-        <Form size="large" className={styles.form} onSubmit={handleSubmit}>
+        <Form
+          size="large"
+          className={styles.form}
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <Segment>
             <Form.Input
               fluid
               icon="mail"
               iconPosition="left"
               name="email"
+              onChange={(event, { name, value }) => {
+                setValue(name, value);
+              }}
               type="email"
+              error={errors.email ? true : false}
               placeholder="Email Adresse"
             />
+            {errors.email && (
+              <div class="ui negative message">
+                Tragen Sie eine gültige E-Mail-Adresse ein
+              </div>
+            )}
             <Form.Input
               fluid
               icon="lock"
               iconPosition="left"
               name="password"
+              onChange={(event, { name, value }) => {
+                setValue(name, value);
+              }}
               type="password"
+              error={errors.password ? true : false}
               placeholder="Passwort"
             />
+            {errors.password && (
+              <div class="ui negative message">
+                Das Passwort muss mindestens 6 Zeichen lang sein
+              </div>
+            )}
             <Button color="purple" fluid size="large">
               Login
             </Button>
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button, Form, Grid, Segment, Message } from "semantic-ui-react";
+import { useForm } from "react-hook-form";
 import styles from "./signup.module.css";
 
 const Signup = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const { register, errors, handleSubmit, setValue } = useForm();
+
+  useEffect(() => {
+    register({ name: "username" }, { required: true, minLength: 3 });
+    register({ name: "email" }, { required: true });
+    register({ name: "password" }, { required: true, minLength: 6 });
+  }, [register]);
+
+  const onSubmit = (data, e) => {
+    console.log(data);
   };
   return (
     <Grid
@@ -18,32 +27,64 @@ const Signup = () => {
           Babbeln
           <span>.io</span>
         </h1>
-        <Form size="large" className={styles.form} onSubmit={handleSubmit}>
+        <Form
+          size="large"
+          className={styles.form}
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <Segment>
             <Form.Input
               fluid
               icon="user"
               iconPosition="left"
               name="username"
+              onChange={(event, { name, value }) => {
+                setValue(name, value);
+              }}
               type="text"
+              error={errors.username ? true : false}
               placeholder="Nutzername"
             />
+            {errors.username && (
+              <div class="ui negative message">
+                Melden Sie sich bei einem gültigen Benutzername an
+              </div>
+            )}
+
             <Form.Input
               fluid
               icon="mail"
               iconPosition="left"
               name="email"
+              onChange={(event, { name, value }) => {
+                setValue(name, value);
+              }}
               type="email"
+              error={errors.email ? true : false}
               placeholder="Email Adresse"
             />
+            {errors.email && (
+              <div class="ui negative message">
+                Tragen Sie eine gültige E-Mail-Adresse ein
+              </div>
+            )}
             <Form.Input
               fluid
               icon="lock"
               iconPosition="left"
               name="password"
+              onChange={(event, { name, value }) => {
+                setValue(name, value);
+              }}
               type="password"
+              error={errors.password ? true : false}
               placeholder="Passwort"
             />
+            {errors.password && (
+              <div class="ui negative message">
+                Das Passwort muss mindestens 6 Zeichen lang sein
+              </div>
+            )}
             <Button color="purple" fluid size="large">
               Anmeldung durchführen
             </Button>
