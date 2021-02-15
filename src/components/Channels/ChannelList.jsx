7453d478d57abea5c1c19b0cb86a6192d93f2d37
sakir tufan <sakirtufan49@gmail.com>
2021-02-15 12:14:45 +0100

    Fix Channel List

--- src/components/Channels/ChannelList.jsx
+++ src/components/Channels/ChannelList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCurrentChannel } from "../../store/actions/channel";
 import { useFirebaseConnect, isLoaded, isEmpty } from "react-redux-firebase";
@@ -11,6 +11,16 @@ const ChannelList = () => {
   const dispatch = useDispatch();
   const channels = useSelector((state) => state.firebase.ordered.channels);
   const currentChannel = useSelector((state) => state.channels.currentChannel)
+  const [mounted, setMounted] = useState(false)
+
+ 
+  useEffect(() => {
+    if (!mounted && !isEmpty(channels)) {
+      const { key, value } = channels[0];
+      setActiveChannel({ key, ...value });
+      setMounted(true);
+    }
+  }, [isLoaded(channels)]);
 
   const setActiveChannel = (channel) => {
     dispatch(setCurrentChannel(channel));
