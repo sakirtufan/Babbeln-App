4bc01cc61031cdc0db22ad3d6ccb311752f19787
sakir tufan <sakirtufan49@gmail.com>
2021-02-15 07:11:29 +0100

    Create a Form to Add a Channel

---
+++ src/components/Channels/CreateChannelForm.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import { Modal, Form, Button } from "semantic-ui-react";
+import { useForm } from "react-hook-form";
+import { useFirebase } from "react-redux-firebase";
+import { useSelector } from "react-redux";
+
+const CreateChannelForm = ({ open, onOpen, onClose }) => {
+  const firebase = useFirebase();
+  const profile = useSelector(state => state.firebase.profile)
+  const { register, errors, handleSubmit, setValue } = useForm();
+
+
+  useEffect(() => {
+    register({ name: "name" }, { required: true });
+    register({ name: "description" }, { required: true, minLength: 20 });
+  }, []);
+
+  const onSubmit = ({ name, description }) => {
+    firebase.push("channels", {
+      name,
+      description,
+      createdBy: {
+        name: profile.name,
+        avatar: profile.avatar
+      }
+    });
+    onClose();
+  }
+
+  return (
+    <Modal open={open} onOpen={onOpen} onClose={onClose}>
+      <Modal.Header>Channels erstellen</Modal.Header>
+      <Modal.Content>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Input
+            fluid
+            icon="hashtag"
+            iconPosition="left"
+            name="name"
+            placeholder="#allgemein"
+            onChange={(e, { name, value }) => {
+              setValue(name,value)
+            }}
+            error={errors.name ? true : false }
+          />
+          <Form.Input
+            fluid
+            icon="hashtag"
+            iconPosition="left"
+            name="description"
+            placeholder="#Es ist ein Channel, in dem jedes allgemeine Thema sprechen kann"
+            onChange={(e, { name, value }) => {
+              setValue(name,value)
+            }}
+            error={errors.description ? true : false }
+          />
+        </Form>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="black" onClick={() => onClose()}>absagen</Button>
+        <Button icon="checkmark" positive onClick={() => handleSubmit(onSubmit)()}>erstellen</Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
+
+export default CreateChannelForm;
